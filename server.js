4f7ebe1b7a20e81944b370b6f0b58dc7bcc4854a
Cisco <francisco-hernandez@lambdastudents.com>
2020-11-02 17:51:19 -0800

    complete

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+const hubsRouter = require('./hubs/hub-router.js')
+
+
+
+server.use(express.json());
+
+server.use('/api/hubs',hubsRouter)
+
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda Hubs API</h>
+      <p>Welcome to the Lambda Hubs API</p>
+    `);
+  });
+  
+ 
+
+  module.exports = server;
